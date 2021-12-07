98a0db6f61dab8936789bc34965e0f3ec9aeba67
endi julian <endi@iglobal.co.id>
2021-12-07 16:40:15 +0700

    main page

---
+++ lib/pages/home/main_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget cartButton() {
+      return FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: secondaryColor,
+        child: Image.asset(
+          'assets/cart_icon.png',
+          width: 20,
+        ),
+      );
+    }
+
+    Widget customBottomNav() {
+      return ClipRRect(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            backgroundColor: backgroundColor4,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/home.png',
+                  width: 21,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/chat_icon.png',
+                  width: 20,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/love_icon.png',
+                  width: 20,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/Profile.png',
+                  width: 18,
+                ),
+                label: '',
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      floatingActionButton: cartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customBottomNav(),
+      body: Center(
+        child: Text('Aku bisaaaaa ayooooo'),
+      ),
+    );
+  }
+}
--- lib/theme.dart
+++ lib/theme.dart
@@ -10,6 +10,7 @@ Color priceColor = Color(0XFF2C96F1);
 Color backgroundColor1 = Color(0XFF1F1D2B);
 Color backgroundColor2 = Color(0XFF2B2937);
 Color backgroundColor3 = Color(0XFF242231);
+Color backgroundColor4 = Color(0XFF252836);
 Color primaryTextColor = Color(0XFFF1F0F2);
 Color secondaryTextColor = Color(0XFF999999);
 Color subtitleColor = Color(0XFF504F5E);
