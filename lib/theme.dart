ff11e77bbfafe3b5aff8e37e3026a2d3f09821a6
endi julian <endi@iglobal.co.id>
2021-12-07 12:52:49 +0700

    email input

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 30.0;
+
 Color primaryColor = Color(0XFF6C5ECF);
 Color secondaryColor = Color(0XFF38ABBE);
 Color alertColor = Color(0XFFED6363);
@@ -10,10 +12,12 @@ Color backgroundColor2 = Color(0XFF2B2937);
 Color backgroundColor3 = Color(0XFF242231);
 Color primaryTextColor = Color(0XFFF1F0F2);
 Color secondaryTextColor = Color(0XFF999999);
+Color subtitleColor = Color(0XFF504F5E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(color: primaryTextColor);
 TextStyle secondaryTextStyle = GoogleFonts.poppins(color: secondaryTextColor);
 TextStyle priceTextStyle = GoogleFonts.poppins(color: priceColor);
+TextStyle subtitleTextStyle = GoogleFonts.poppins(color: subtitleColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
