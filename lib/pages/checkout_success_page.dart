e4a6b33cb3f2548852eb0f06ae1943f7ef09110c
endi julian <endi@iglobal.co.id>
2021-12-22 10:34:55 +0700

    checkout page

---
+++ lib/pages/checkout_success_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CheckoutSuccessPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    header() {
+      return AppBar();
+    }
+
+    return Scaffold(
+      appBar: header(),
+    );
+  }
+}
---
+++ lib/widgets/checkout_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class CheckoutCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 12),
+      padding: EdgeInsets.symmetric(
+        vertical: 20,
+        horizontal: 12,
+      ),
+      decoration: BoxDecoration(
+        color: backgroundColor4,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: AssetImage('assets/imgae_shoes.png'),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Terrex Urban Low Terrex Urban LowTerrex Urban LowTerrex Urban Low',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: semiBold,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  '\$143,98',
+                  style: priceTextStyle.copyWith(
+                    fontWeight: regular,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Text(
+            '2 Items',
+            style: secondaryTextStyle.copyWith(
+              fontSize: 12,
+              fontWeight: regular,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
